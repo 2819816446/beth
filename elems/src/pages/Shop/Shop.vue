aecea39d6e76b994d2fac4ed21a69ce64645fe6c
余祚镪 <2819816446@qq.com>
2018-06-11 16:33:12 +0800

    修改不使用多级路由。

--- elems/src/pages/Shop/Shop.vue
+++ elems/src/pages/Shop/Shop.vue
@@ -1,7 +1,16 @@
 <template>
 	<div class="shop_wrap">
+		<div class="shop_hd">
+			<div class="shop_hd_1">
+				<img src="../../assets/imgage/m2/m2_1.jpeg" height="681" width="1024"  alt="">
+				<div class="msg">
+					<h2 class="hd_title">店铺名称</h2>
+					<p class="descript_text">商家配送／分钟送达／配送费¥5</p>
+					<p class="p_info">公告 </p>
+				</div>
+			</div>
+		</div>
 		
-		<p>shop</p>
 	</div>
 </template>
 
@@ -57,4 +66,10 @@
 
 <style>
 	.shop_wrap{width: 100%;position: relative;}
+	.shop_hd{width: 100%;height: 8rem;border-bottom: 1px solid red;}
+	.shop_hd_1{height: 6rem;padding:0.5rem;box-sizing: border-box;display:-webkit-box;display:-webkit-flex;display:flex; display:-moz-box;}
+	.shop_hd_1 img{width: 5rem;height: 5rem;border-radius: 0.5rem;}
+	.shop_hd_1 .msg{-webkit-box-flex: 1;-webkit-flex: 1;flex: 1;box-sizing: border-box;height: 5rem;position: relative;margin-left: 0.5rem;}
+	.msg h2{font-size: 1.3rem;line-height: 2rem;}
+	.msg .p_info{white-space:nowrap;overflow:hidden;text-overflow:ellipsis;width: 100%;}
 </style>
--- elems/src/router/index.js
+++ elems/src/router/index.js
@@ -26,55 +26,97 @@ Vue.use(Router)
 
   const routes = [
     // 首页 index.html
-    {
-      path: '/',
-      name: 'App',
-      component: App,
-      children:[		//二级路由
-      	{
-      		path:'/',
-      		redirect:'/home'
-      	},	
-      	{
-      		path:'/home',
-      		name:'Home',
-      		component:Home
-      	},
-      	{
-      		path:'cities/:id',
-      		name:'SelectCity',
-      		component:SelectCity
-      	},
-        {
-          path:'/search',
-          name:'Search',
-          component:Search
-        },
-        {
-          path:'/mysite',
-          name:'MySite',
-          component:MySite,
-          children:[        //首页的二级路由
-            {
-              path:'shop',
-              name:'Shop',
-              component:Shop
-            },
-          ]
-        },
-        {
-          path:'/order',
-          name:'OrderList',
-          component:OrderList
-        },
-        {
-          path:'/profile',
-          name:'Profile',
-          component:Profile
-        }
+    // {
+    //   path: '/',
+    //   name: 'App',
+    //   component: App,
+    //   children:[		//二级路由
+    //   	{
+    //   		path:'/',
+    //   		redirect:'/home'
+    //   	},	
+    //   	{
+    //   		path:'/home',
+    //   		name:'Home',
+    //   		component:Home
+    //   	},
+    //   	{
+    //   		path:'cities/:id',
+    //   		name:'SelectCity',
+    //   		component:SelectCity
+    //   	},
+    //     {
+    //       path:'/search',
+    //       name:'Search',
+    //       component:Search
+    //     },
+    //     {
+    //       path:'/mysite',
+    //       name:'MySite',
+    //       component:MySite,
+    //       children:[        //首页的二级路由
+    //         {
+    //           path:'shop',
+    //           name:'Shop',
+    //           component:Shop
+    //         },
+    //       ]
+    //     },
+    //     {
+    //       path:'/order',
+    //       name:'OrderList',
+    //       component:OrderList
+    //     },
+    //     {
+    //       path:'/profile',
+    //       name:'Profile',
+    //       component:Profile
+    //     }
 
-      ]
-    }, 
+    //   ]
+    // }, 
+
+    // 取消APP 级别的路由
+
+    {
+      path:'/',
+      redirect:'/home'
+    },  
+    {
+      path:'/home',
+      name:'Home',
+      component:Home
+    },
+    {
+      path:'cities/:id',
+      name:'SelectCity',
+      component:SelectCity
+    },
+    {
+      path:'/search',
+      name:'Search',
+      component:Search
+    },
+    {
+      path:'/mysite',
+      name:'MySite',
+      component:MySite
+    },
+    {
+      path:'/shop',
+      name:'Shop',
+      component:Shop
+    },    
+    {
+      path:'/order',
+      name:'OrderList',
+      component:OrderList
+    },
+    {
+      path:'/profile',
+      name:'Profile',
+      component:Profile
+    }   
   ]
 
 
